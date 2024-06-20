a424b455fb4a739016f7865fac0cc4b93ffd5242
Galen Zhao <github@zhenggangzhao.org>
2024-06-20 17:39:51 +0800

    wip

--- app/src/main/java/me/hufman/androidautoidrive/carapp/carinfo/CarInformationDiscoveryService.kt
+++ app/src/main/java/me/hufman/androidautoidrive/carapp/carinfo/CarInformationDiscoveryService.kt
@@ -19,14 +19,14 @@ import me.hufman.androidautoidrive.MutableAppSettingsReceiver
 import me.hufman.androidautoidrive.carapp.CarAppService
 import me.hufman.androidautoidrive.cds.CDSConnection
 import me.hufman.androidautoidrive.cds.CDSConnectionAsync
-import org.prowl.torque.remote.ITorqueService
+//import org.prowl.torque.remote.ITorqueService
 import java.util.Timer
 
 class CarInformationDiscoveryService: CarAppService() {
 	val appSettings by lazy { MutableAppSettingsReceiver(applicationContext) }
 	var carappCapabilities: CarInformationDiscovery? = null
 
-	private var torqueService: ITorqueService? = null
+//	private var torqueService: ITorqueService? = null
 	private val updateTimer: Timer? = null
 
 	override fun shouldStartApp(): Boolean {
@@ -104,45 +104,45 @@ class CarInformationDiscoveryService: CarAppService() {
 	 */
 	private val connection: ServiceConnection = object : ServiceConnection {
 		override fun onServiceConnected(arg0: ComponentName, service: IBinder) {
-			torqueService = ITorqueService.Stub.asInterface(service)
-			try {
-				// Bind to the torque service
-
-				if (torqueService!!.getVersion() < 19) {
-//					popupMessage(
-//						"Incorrect version",
-//						"You are using an old version of Torque with this plugin.\n\nThe plugin needs the latest version of Torque to run correctly.\n\nPlease upgrade to the latest version of Torque from Google Play",
-//						true
-//					)
-					return
-				}
-				// String used for code readability.
-				var text = ""
-				try {
-					text = """
-	      	${text}API Version: ${torqueService!!.version}
-	      	
-	      	""".trimIndent()
-				} catch(e: RemoteException) {
-					Log.e(javaClass.canonicalName, e.message, e)
-				}
-				tip(text)
-			} catch (e: RemoteException) {
-				Log.e(javaClass.canonicalName, e.message, e)
-			}
+//			torqueService = ITorqueService.Stub.asInterface(service)
+//			try {
+//				// Bind to the torque service
+//
+//				if (torqueService!!.getVersion() < 19) {
+////					popupMessage(
+////						"Incorrect version",
+////						"You are using an old version of Torque with this plugin.\n\nThe plugin needs the latest version of Torque to run correctly.\n\nPlease upgrade to the latest version of Torque from Google Play",
+////						true
+////					)
+//					return
+//				}
+//				// String used for code readability.
+//				var text = ""
+//				try {
+//					text = """
+//	      	${text}API Version: ${torqueService!!.version}
+//
+//	      	""".trimIndent()
+//				} catch(e: RemoteException) {
+//					Log.e(javaClass.canonicalName, e.message, e)
+//				}
+//				tip(text)
+//			} catch (e: RemoteException) {
+//				Log.e(javaClass.canonicalName, e.message, e)
+//			}
 		}
 
 		override fun onServiceDisconnected(name: ComponentName) {
-			torqueService = null
+//			torqueService = null
 		}
 	}
 	override fun onCarStop() {
 		if (updateTimer != null) updateTimer.cancel()
 		try {
 			tip("Press menu for options")
-			if(torqueService!=null) {
-				unbindService(connection)
-			}
+//			if(torqueService!=null) {
+//				unbindService(connection)
+//			}
 		} catch (e: RuntimeException) {
 			Log.e(javaClass.canonicalName, e.message, e)
 		}
